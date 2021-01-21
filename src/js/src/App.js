87d6cae8296f6942e9ab5d8d5611dcc0ccb224a8
elarsaks <elarsaks@gmail.com>
2021-01-21 19:29:00 +0200

    Connect client to API.

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { getAllStudents } from "./client";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    getAllStudents().then((res) => {
+      res.json().then((students) => {
+        console.log(students);
+      });
+    });
+    return <h1> Hello world!</h1>;
+  }
 }
 
 export default App;
--- src/main/java/Javareactcourse/javareactcourse/StudentController.java
+++ src/main/java/Javareactcourse/javareactcourse/StudentController.java
@@ -3,12 +3,14 @@ package Javareactcourse.javareactcourse;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("student")
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("students")
 public class StudentController {
 	@GetMapping
 	public List<Student> getAllStudents(){
